d710504530ec3c2f408ac54772f4c902dc6e6640
HT <HT@HanSiro>
2014-11-18 13:12:56 +0700

    sdfdsf

---
+++ src/a/a.java
@@ -0,0 +1,12 @@
+package a;
+
+public class a {
+
+	public static void main(String[] args) {
+		System.out.println("a");
+		System.out.println("a");
+		System.out.println("a");
+		System.out.println("a");
+		System.out.println("a");
+	}
+}
